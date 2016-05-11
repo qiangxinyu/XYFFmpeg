de725dd545db0d72727c6c9ebcb12f2ed5363590
强新宇 <qiangxinyu@qiangxinyudemacbook-pro.local>
2016-05-11 15:01:23 +0800

    Initial Commit

---
+++ XYFFmpeg/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  XYFFmpeg
+//
+//  Created by Xinyu Qiang on 16/5/11.
+//  Copyright © 2016年 Xinyu Qiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ XYFFmpeg/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  XYFFmpeg
+//
+//  Created by Xinyu Qiang on 16/5/11.
+//  Copyright © 2016年 Xinyu Qiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
